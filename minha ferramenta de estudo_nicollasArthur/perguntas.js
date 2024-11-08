c65f75d5eaa014939f29d0a7a91195fc8cf0e09a
The king von <00001094887456sp@al.educacao.sp.gov.br>
2024-11-08 11:28:25 -0300

    Add files via upload

---
+++ minha ferramenta de estudo_nicollasArthur/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é call of duty?',
+    'o Call of Duty é um fps focado em batlle royale'
+)
+
+criaCartao(
+    'fisica',
+    'O que a primeira lei de newton diz?',
+    'Um objeto em repouso permanece em repouso, ou se estiver em movimento, permanece em movimento com velocidade constante, a menos que uma força externa atue sobre ele.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'matematica',
+    'qual a formula de bhaskara?',
+    'a formula é (-b±√(b²-4ac))/(2a) .'
+)
